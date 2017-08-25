de3f56c4035a2258957aff3e8e8879751c2bb937
Yuki Ikeda <yuki.jayce.ikeda@gmail.com>
2017-08-25 14:31:01 +1200

    username registration fixed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:user_name, :password]
+    added_attrs = [:email,:user_name, :password]
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
