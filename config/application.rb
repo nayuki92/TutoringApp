a3f0c9ad2cc07de90bc9b58dbda1b686354d27ef
Yuki Ikeda <yuki.jayce.ikeda@gmail.com>
2017-08-25 14:42:00 +1200

    application.rb fixed for ck-editor.

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module Sakurabo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
 
-config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w( ckeditor/* )
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
